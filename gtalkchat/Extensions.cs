1afccc218abe44859d5895a3c6d709c16411757a
Juliana Pena <limited.mage@gmail.com>
2011-07-23 14:53:37 -0700

    Added sorting extension to ObservableCollection

--- gtalkchat/Extensions.cs
+++ gtalkchat/Extensions.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System;
+using System.Linq;
 
 namespace gtalkchat
 {
     public static class Extensions
     {
-        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> obj)
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
         {
             var o = new ObservableCollection<T>();
 
-            foreach (T item in obj)
+            foreach (T item in collection)
             {
                 o.Add(item);
             }
 
             return o;
+        }
+
+        public static void Sort<T>(this ObservableCollection<T> collection)
+        {
+            collection.Sort(null);
+        }
+
+        public static void Sort<T>(this ObservableCollection<T> collection, Comparison<T> comparison)
+        {
+            List<T> sorted = collection.ToList();
+
+            if (comparison != null)
+            {
+                sorted.Sort(comparison);
+            }
+            else
+            {
+                sorted.Sort();
+            }
+
+            collection.Clear();
+
+            foreach (T item in sorted)
+            {
+                collection.Add(item);
+            }
         }
     }
 }
