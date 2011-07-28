3d602d6a7dcb80d3c92cf398172ad679158c7850
Juliana Pena <limited.mage@gmail.com>
2011-07-28 00:03:42 -0700

    Buttons change when using OTR Also, fixed some problems when scrolling non-full-height elements.

--- gtalkchat/ChatPage.xaml.cs
+++ gtalkchat/ChatPage.xaml.cs
@@ -137,10 +137,16 @@ namespace gtalkchat {
         }
 
         private void ShowStartOtr() {
+            ApplicationBarIconButton otrButton = (ApplicationBarIconButton)ApplicationBar.Buttons[1];
+            otrButton.IconUri = new Uri("/icons/appbar.unlock.rest.png", UriKind.Relative);
+            otrButton.Text = "end otr";
             LogChatEvent("This conversation is now off the record.");
         }
 
         private void ShowEndOtr() {
+            ApplicationBarIconButton otrButton = (ApplicationBarIconButton)ApplicationBar.Buttons[1];
+            otrButton.IconUri = new Uri("/icons/appbar.lock.rest.png", UriKind.Relative);
+            otrButton.Text = "go otr";
             LogChatEvent("This conversation is no longer off the record.");
         }
 
@@ -334,7 +340,6 @@ namespace gtalkchat {
         }
 
         private void OTRButton_Click(object sender, EventArgs e) {
-            // TODO: Change icons
             if (otr) {
                 gtalk.OTR(email, false, s => Dispatcher.BeginInvoke(ShowEndOtr), s => { });
                 otr = false;
