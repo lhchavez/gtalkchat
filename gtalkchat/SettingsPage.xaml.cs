b16fc6e39e3c7d608bd1652e08c6c1644c1ebd10
lhchavez <lhchavez@lhchavez.com>
2011-08-07 01:42:44 -0500

    Notification settings.

--- gtalkchat/SettingsPage.xaml.cs
+++ gtalkchat/SettingsPage.xaml.cs
@@ -13,6 +13,9 @@ namespace gtalkchat {
 
             RagesCheckbox.IsChecked = App.Current.Settings.Contains("rages") && (bool)App.Current.Settings["rages"];
             LandscapeCheckbox.IsChecked = App.Current.Settings.Contains("rotate") && (bool)App.Current.Settings["rotate"];
+            ToastCheckbox.IsChecked = !App.Current.Settings.Contains("toastNotification") || (bool)App.Current.Settings["toastNotification"];
+            TileCheckbox.IsChecked = !App.Current.Settings.Contains("tileNotification") || (bool)App.Current.Settings["tileNotification"];
+            SecondaryTileCheckbox.IsChecked = !App.Current.Settings.Contains("secondaryTileNotification") || (bool)App.Current.Settings["secondaryTileNotification"];
         }
 
         private void RagesCheckbox_Checked(object sender, RoutedEventArgs e) {
@@ -21,6 +24,20 @@ namespace gtalkchat {
 
         private void LandscapeCheckbox_Checked(object sender, RoutedEventArgs e) {
             App.Current.Settings["rotate"] = LandscapeCheckbox.IsChecked;
+        }
+
+        private void Notification_Checked(object sender, RoutedEventArgs e) {
+            App.Current.GtalkClient.Notifications(
+                ToastCheckbox.IsChecked.Value,
+                TileCheckbox.IsChecked.Value,
+                SecondaryTileCheckbox.IsChecked.Value,
+                data => Dispatcher.BeginInvoke(() => { 
+                    App.Current.Settings["toastNotification"] = ToastCheckbox.IsChecked;
+                    App.Current.Settings["tileNotification"] = TileCheckbox.IsChecked;
+                    App.Current.Settings["secondaryTileNotification"] = SecondaryTileCheckbox.IsChecked;
+                }), error => {
+                }
+            );
         }
     }
 }
