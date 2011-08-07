842b093f9aa62d2943d91df67dc1b831bbc199f6
lhchavez <lhchavez@lhchavez.com>
2011-08-07 17:37:35 -0500

    Fixed a race condition in the settings page.

--- gtalkchat/SettingsPage.xaml.cs
+++ gtalkchat/SettingsPage.xaml.cs
@@ -3,6 +3,8 @@ using Microsoft.Phone.Controls;
 
 namespace gtalkchat {
     public partial class SettingsPage : PhoneApplicationPage {
+        private bool fireEvents;
+
         public SettingsPage() {
             InitializeComponent();
         }
@@ -11,26 +13,36 @@ namespace gtalkchat {
             base.OnNavigatedTo(e);
             App.Current.GtalkHelper.SetCorrectOrientation(this);
 
+            fireEvents = false;
+
             RagesCheckbox.IsChecked = App.Current.Settings.Contains("rages") && (bool)App.Current.Settings["rages"];
             LandscapeCheckbox.IsChecked = App.Current.Settings.Contains("rotate") && (bool)App.Current.Settings["rotate"];
             ToastCheckbox.IsChecked = !App.Current.Settings.Contains("toastNotification") || (bool)App.Current.Settings["toastNotification"];
             TileCheckbox.IsChecked = !App.Current.Settings.Contains("tileNotification") || (bool)App.Current.Settings["tileNotification"];
             SecondaryTileCheckbox.IsChecked = !App.Current.Settings.Contains("secondaryTileNotification") || (bool)App.Current.Settings["secondaryTileNotification"];
+
+            fireEvents = true;
         }
 
         private void RagesCheckbox_Checked(object sender, RoutedEventArgs e) {
+            if (!fireEvents) return;
+
             App.Current.Settings["rages"] = RagesCheckbox.IsChecked;
         }
 
         private void LandscapeCheckbox_Checked(object sender, RoutedEventArgs e) {
+            if (!fireEvents) return;
+
             App.Current.Settings["rotate"] = LandscapeCheckbox.IsChecked;
         }
 
         private void Notification_Checked(object sender, RoutedEventArgs e) {
+            if (!fireEvents) return;
+
             App.Current.GtalkClient.Notifications(
-                ToastCheckbox.IsChecked.Value,
-                TileCheckbox.IsChecked.Value,
-                SecondaryTileCheckbox.IsChecked.Value,
+                ToastCheckbox.IsChecked.GetValueOrDefault(true),
+                TileCheckbox.IsChecked.GetValueOrDefault(true),
+                SecondaryTileCheckbox.IsChecked.GetValueOrDefault(true),
                 data => Dispatcher.BeginInvoke(() => { 
                     App.Current.Settings["toastNotification"] = ToastCheckbox.IsChecked;
                     App.Current.Settings["tileNotification"] = TileCheckbox.IsChecked;
