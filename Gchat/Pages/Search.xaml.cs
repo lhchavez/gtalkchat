bf554b8cf11b9738e85c12e5ee6d262abd8b19fb
Juliana Pena <limited.mage@gmail.com>
2011-10-16 15:44:38 -0500

    Experimenting with ways to improve search performance

--- Gchat/Pages/Search.xaml.cs
+++ Gchat/Pages/Search.xaml.cs
@@ -11,13 +11,23 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Gchat.Data;
+using System.Windows.Threading;
+using System.ComponentModel;
 
 namespace Gchat.Pages {
     public partial class Search : PhoneApplicationPage {
+        private string oldSearch;
+
         public Search() {
             InitializeComponent();
-
             SearchResults.ItemsSource = App.Current.Roster;
+
+            oldSearch = string.Empty;
+
+            DispatcherTimer t = new DispatcherTimer();
+            t.Tick += (s, e) => FilterListForSearch();
+            t.Interval = new TimeSpan(1000);
+            t.Start();
         }
 
         private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e) {
@@ -52,17 +62,29 @@ namespace Gchat.Pages {
         }
 
         private void SearchBox_KeyUp(object sender, KeyEventArgs e) {
-            var results = new List<Contact>();
-
-            var search = SearchBox.Text.ToLower();
-
-            foreach (var contact in App.Current.Roster) {
-                if (contact.Matches(search)) {
-                    results.Add(contact);
-                }
-            }
+            //FilterListForSearch();
             
-            SearchResults.ItemsSource = results;
+        }
+
+        private void FilterListForSearch() {
+            Dispatcher.BeginInvoke(() => {
+                var search = SearchBox.Text.ToLower();
+                if (oldSearch != search) {
+                    oldSearch = search;
+
+                    BackgroundWorker w = new BackgroundWorker();
+                    w.DoWork += (s, e) => {
+                        var results = new List<Contact>();
+                        foreach (var contact in App.Current.Roster) {
+                            if (contact.Matches(search)) {
+                                results.Add(contact);
+                            }
+                        }
+                        Dispatcher.BeginInvoke(() => SearchResults.ItemsSource = results);
+                    };
+                    w.RunWorkerAsync();
+                }
+            });
         }
     }
 }
