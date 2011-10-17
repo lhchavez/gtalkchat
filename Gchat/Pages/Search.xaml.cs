8b7746c62b0c07864e71ad4b39c9dfc89853eaca
Juliana Pena <limited.mage@gmail.com>
2011-10-16 20:53:24 -0500

    Fixed #62

--- Gchat/Pages/Search.xaml.cs
+++ Gchat/Pages/Search.xaml.cs
@@ -27,25 +27,6 @@ namespace Gchat.Pages {
             SearchBox.Focus();
         }
 
-        private void PinContact_Click(object sender, RoutedEventArgs e) {
-            MenuItem item = sender as MenuItem;
-            Contact c = item.DataContext as Contact;
-
-            App.Current.GtalkHelper.PinContact(c.Email);
-        }
-
-        private void ContextMenu_Opened(object sender, RoutedEventArgs e) {
-            ContextMenu menu = sender as ContextMenu;
-            Contact c = menu.DataContext as Contact;
-            MenuItem item = menu.Items[0] as MenuItem;
-
-            if (App.Current.GtalkHelper.IsContactPinned(c.Email)) {
-                item.IsEnabled = false;
-            } else {
-                item.IsEnabled = true;
-            }
-        }
-
         private void SearchResults_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             if (e.AddedItems.Count > 0) {
                 var to = (e.AddedItems[0] as Contact).Email;
