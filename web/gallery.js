d3fefb72f74274c9cfc815003283114cc6f41de1
Juliana Pena <limited.mage@gmail.com>
2011-10-21 16:16:25 -0500

    Experimenting with new web layout

--- web/gallery.js
+++ web/gallery.js
@@ -12,4 +12,24 @@ $(function() {
    if (!$.browser.msie || parseInt($.browser.version, 10) >= 9) {
       setTimeout(rotateScreenshots, 3000);
    }
+   
+   
+   $("#nav-features").click(function() {
+      $("#features").show();
+      $("#reviews").hide();
+      $("#support").hide();
+   });
+   
+   $("#nav-reviews").click(function() {
+      $("#features").hide();
+      $("#reviews").show();
+      $("#support").hide();
+   });
+   
+   $("#nav-support").click(function() {
+      $("#features").hide();
+      $("#reviews").hide();
+      $("#support").show();
+   });
+
 });
