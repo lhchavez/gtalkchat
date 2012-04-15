df14c9f99a6c7a80e991d933f380a2a387036168
Juliana Pena <limited.mage@gmail.com>
2012-04-15 15:25:59 -0500

    Fixed #113

--- Gchat/Pages/Search.xaml.cs
+++ Gchat/Pages/Search.xaml.cs
@@ -71,5 +71,11 @@ namespace Gchat.Pages {
                 }
             });
         }
+
+        private void SearchBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e) {
+            if (e.Key == System.Windows.Input.Key.Enter) {
+                this.Focus();
+            }
+        }
     }
 }
